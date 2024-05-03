498f699bf1b4f86a9a93601f513eb2f8e644913e
Archie Tripathi <archie.webwork@gmail.com>
2024-05-03 08:19:40 +0530

    Making expense sharing easier

---
+++ src/main/java/dev/archie/ExpenseShare/ExpenseShareApplication.java
@@ -0,0 +1,13 @@
+package dev.archie.ExpenseShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseShareApplication.class, args);
+	}
+
+}
